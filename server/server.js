9434ddd287135f76fa946225c483ca302fd5dd6b
pastendi <109973861+pastendi@users.noreply.github.com>
2023-08-26 10:42:21 +0545

    MERN setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import 'express-async-errors'
+import connectDB from './connectDB.js'
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+const port = process.env.PORT || 5000
+const start = async () => {
+  try {
+    connectDB()
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
